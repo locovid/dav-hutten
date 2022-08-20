a19565f74c32136aa26342dfa95a049f52746470
leidy <leidy.garzon@check24.de>
2022-08-20 16:32:01 +0200

    Check availability every 5 mins

--- app.js
+++ app.js
@@ -1,44 +1,19 @@
-import { getReservations, getSessionId } from "./src/clients/davClient.js";
+import { getHutsAvailabilities } from "./src/services/hutService.js";
 import { huts } from "./src/models/huts.js";
+const intervalToCheckInMins = 5;
 
-const hutsToWatch = [huts.knorrHut, huts.reintalangerHut];
-const datesToWatch = ["02.09.2022"];
+setInterval(async function () {
+  const hutsToWatch = [huts.knorrHut, huts.reintalangerHut];
+  const datesToWatch = ["02.09.2022"];
 
-const availability = await Promise.all(
-  hutsToWatch.map(async (hut) => {
-    const sessionId = await getSessionId(hut.id);
-    const reservationsObj = await getReservations({
-      initialDate: datesToWatch[0], // Todo: iterate peer dates
-      sessionId,
-    });
+  const availability = await getHutsAvailabilities(hutsToWatch, datesToWatch);
 
-    // TODO: filter by date, currently i know the first result its the given date
-    const availabilitiesForDate = Object.values(
-      Object.values(reservationsObj)[0]
-    );
-
-    const availableBeds = availabilitiesForDate
-      .filter(
-        (hutBed) => hutBed.bedCategoryType === "ROOM" && hutBed.freeRoom > 0
-      )
-      .map((hutBed) => ({
-        bedCategoryId: hutBed.bedCategoryId,
-        freeRoom: hutBed.freeRoom,
-      }));
-
-    if (!availableBeds.length) {
-      return null;
-    }
-
-    return {
-      hutName: hut.name,
-      availableBeds: availableBeds.flatMap((availableBed) => availableBed),
-    };
-  })
-);
-
-const results = availability.filter((a) => !!a);
-if (results.length) {
-  console.log("Reserve!");
-  console.log(JSON.stringify(result));
-}
+  const results = availability.filter((a) => !!a);
+  if (results.length) {
+    console.log("Reserve!");
+    console.log(JSON.stringify(results));
+  }
+  if (results.length === 2) {
+    console.log("knorrHut available!!!!");
+  }
+}, 60 * 1000 * intervalToCheckInMins);
--- src/services/hutService.js
+++ src/services/hutService.js
@@ -1,7 +1,38 @@
-import { huts } from "../models/huts";
+import { getReservations, getSessionId } from "./../clients/davClient.js";
 
-function getFavoriteHuts() {
-  return huts;
+export async function getHutsAvailabilities(hutsToWatch, datesToWatch) {
+  const availabilities = await Promise.all(
+    hutsToWatch.map(async (hut) => {
+      const sessionId = await getSessionId(hut.id);
+      const reservationsObj = await getReservations({
+        initialDate: datesToWatch[0], // Todo: iterate peer dates
+        sessionId,
+      });
+
+      // TODO: filter by date, currently i know the first result its the given date
+      const availabilitiesForDate = Object.values(
+        Object.values(reservationsObj)[0]
+      );
+
+      const availableBeds = availabilitiesForDate
+        .filter(
+          (hutBed) => hutBed.bedCategoryType === "ROOM" && hutBed.freeRoom > 0
+        )
+        .map((hutBed) => ({
+          bedCategoryId: hutBed.bedCategoryId,
+          freeRoom: hutBed.freeRoom,
+        }));
+
+      if (!availableBeds.length) {
+        return null;
+      }
+
+      return {
+        hutName: hut.name,
+        availableBeds: availableBeds.flatMap((availableBed) => availableBed),
+      };
+    })
+  );
+
+  return availabilities;
 }
-
-function getSetSessionIds(huts) {}
